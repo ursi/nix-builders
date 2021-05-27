aad61783c82cac2b6f5dbbee525c12360c76f7a9
Mason Mackaman <masondeanm@aol.com>
2021-05-27 17:47:58 -0400

    Make compatible with nix stable

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ pkgs, system }:
+    let p = pkgs; in
+    rec
+    { write-js-file =
+        { name
+        , js
+        , node ? p.nodejs
+        , destination ? ""
+        }:
+        let
+          js' =
+            if node == null then js
+              js
+            else
+              ''
+              #! ${node}/bin/node
+              ${js}
+              '';
+        in
+        p.writeTextFile
+          { name = name;
+            text = js';
+            executable = !builtins.isNull node;
+            inherit destination;
+            # TODO: add checkPhase
+          };
+
+      write-js-script = name: js: write-js-file { inherit name js; };
+
+      write-js-script-bin = name: js:
+        write-js-file { inherit name js; destination = "/bin/${name}"; };
+    }
